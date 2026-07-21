2984cfe881a8dd4a9e869afd9228132e333d9cf4
Joseph Wang <ivan_whisper@outlook.com>
2026-07-21 11:32:53 +0800

    add dto

--- dto/message_dto.go
+++ dto/message_dto.go
@@ -1,5 +1,36 @@
 package dto
 
-type MessageBatcExecRequest struct {
-	Batch int64 `json:"batch" form:"batch" url:"batch" uri:"uri"`
+import (
+	acDto "github.com/illidaris/aphrodite/dto"
+)
+
+type MessagePageListRequest struct {
+	acDto.Page
+	BizId int64
+	Busi  int64
+	Topic string
+	Group []string
+	Ids   []string
+}
+
+type ExecResult struct {
+	Id      string
+	Success bool
+	ErrMsg  string
+}
+type MessageBatchExecResult struct {
+	Data []ExecResult `json:"data"`
+}
+type MessageBatchExecResponse struct {
+	acDto.BaseResponse
+	Data MessageBatchExecResult `json:"data"`
+}
+
+type MessagePageListResponse struct {
+	acDto.BaseResponse
+	Data acDto.GenericPager[*Message] `json:"data"`
+}
+
+type MessagePushRequest struct {
+	Msgs []Message `json:"msgs"`
 }
